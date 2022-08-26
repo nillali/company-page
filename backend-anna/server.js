c23c212491df841b126da4a6ad1b52130895c4c0
Pernilla Lindström <pernilla_lindstrom@outlook.com>
2022-08-26 10:53:06 +0200

    commented path

--- backend-anna/server.js
+++ backend-anna/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // const favicon = require('serve-favicon')
-const path = require('path')
+// const path = require('path')
 
 const PORT = process.env.PORT || 3000
 
